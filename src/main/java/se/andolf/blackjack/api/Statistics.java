0f6414f3f835fcf58ce51d8b072b39a16c8d75e6
Thomas Andolf <thomas.andolf@gmail.com>
2017-10-21 16:43:46 +0200

    added some basic printing

--- src/main/java/se/andolf/blackjack/api/Statistics.java
+++ src/main/java/se/andolf/blackjack/api/Statistics.java
@@ -1,9 +1,15 @@
 package se.andolf.blackjack.api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * @author Thomas on 2017-09-16.
  */
-public class Statistics extends AbstractStatistic{
+public class Statistics extends AbstractStatistic {
+
+    private static final Logger logger = LogManager.getLogger(Statistics.class);
+
 
     private PlayerStatistic dealer;
 
@@ -13,5 +19,17 @@ public class Statistics extends AbstractStatistic{
 
     public PlayerStatistic getDealer() {
         return dealer;
+    }
+
+    @Override
+    public String toString() {
+        return "Statistics { " +
+                "wins = " + getWins() +
+                ", losses = " + getLosses() +
+                ", rounds = " + getRounds() + " }";
+    }
+
+    public void print() {
+        logger.info(toString());
     }
 }
